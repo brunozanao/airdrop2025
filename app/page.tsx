d3e70769a94026ef60c7013fdbd26d077784769a
brunozanao <brunovinicius_2012@hotmail.com>
2024-07-10 11:00:37 -0300

    commit 4

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Web3ConnectButton from "./_components/Web3ConnectButton";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p> Carbon.Std | 𝗦𝘂𝘀𝘁𝗮𝗶𝗻𝗮𝗯𝗶𝗹𝗶𝘁𝘆 𝗕𝗼𝗻𝗱𝘀 + 𝗡𝗙𝗧 = 𝗥𝗲𝗙𝗶</p>
       <h1 className="text-2xl font-bold">Carbon.Std | 𝗦𝘂𝘀𝘁𝗮𝗶𝗻𝗮𝗯𝗶𝗹𝗶𝘁𝘆 𝗕𝗼𝗻𝗱𝘀 + 𝗡𝗙𝗧 = 𝗥𝗲𝗙𝗶
         </h1>
         <p className="px-8 mt-8 mb-12 text-lg">The Future Is Green, and Goes Beyond the Carbon Market and Blockchain.</p>
